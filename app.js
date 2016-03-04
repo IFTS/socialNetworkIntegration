28e0875dceb7a293fb1fca9590324c63f69b9741
Nick Liffen <UP653591@myport.ac.uk>
2016-03-04 12:18:14 +0000

    Working with ES2015

--- app.js
+++ app.js
@@ -1,60 +1,55 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+"use strict";
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+//Import all node modules
+import express from 'express';
+import passport from 'passport';
+import flash from 'connect-flash';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import session from 'express-session';
+import favicon from 'serve-favicon';
+import dotenv from 'dotenv';
 
-var app = express();
+//Load the .env file from root directory.
+dotenv.load();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+//Puts express onto the application
+const app  = express();
+//Port which the node server will run on.
+const port = process.env.PORT || 8080;
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//Load my SQL connection file
+//var connection = require('./config/connection.js');
+//Load my SendGrid Connection file
+//var sendgridClient = require('./config/emailConnection.js');
+//Load my Twilio connection file
+//var twilioClient = require('./config/textConnection.js');
 
-app.use('/', routes);
-app.use('/users', users);
+//Pass Information to our Passport File for Authentication
+//require('./config/passport')(passport, connection); // pass passport & for configuration
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
 
-// error handlers
+//Setting our Template Engine to EJS
+app.set('view engine', 'ejs'); // set up ejs for templating
+app.use(express.static(__dirname + '/public'));
+//app.use('/vendor', express.static(__dirname + '/bower_components'));
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+// required for passport
+app.use(session({ secret: 'bobsyouruncle', cookie: {maxAge: 900000000000000}})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 
-module.exports = app;
+//Passing our Passport Information and Application Information to our routes
+require('./app/routes.js')(app, passport);
+
+app.listen(port);
+console.log('Server connection established');
+console.log('Using The Following Port: ' + port);

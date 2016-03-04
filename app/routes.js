30ce5e8b1bec5549c54094c92bad7ec2e03daf9e
Nick Liffen <UP653591@myport.ac.uk>
2016-03-04 12:29:32 +0000

    First Proper Commit

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,10 @@
 "use strict";
 module.exports = function(app, passport) {
 
+  app.get('/', function(req, res) {
+    res.render('index.ejs', {
+            title: "Hello There"
+        }); // load the login.ejs file
+      });
 
 };

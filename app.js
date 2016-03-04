2e23f32c3b18ceacd0ca4184584e3da75102d44e
Nick Liffen <UP653591@myport.ac.uk>
2016-03-04 22:12:19 +0000

    Basic OAuth Login: Facebook, Twitter, Instagram

--- app.js
+++ app.js
@@ -10,34 +10,31 @@ import bodyParser from 'body-parser';
 import session from 'express-session';
 import favicon from 'serve-favicon';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 //Load the .env file from root directory.
 dotenv.load();
-
 //Puts express onto the application
 const app  = express();
 //Port which the node server will run on.
 const port = process.env.PORT || 8080;
 
-//Load my SQL connection file
-//var connection = require('./config/connection.js');
-//Load my SendGrid Connection file
-//var sendgridClient = require('./config/emailConnection.js');
-//Load my Twilio connection file
-//var twilioClient = require('./config/textConnection.js');
-
-//Pass Information to our Passport File for Authentication
-//require('./config/passport')(passport, connection); // pass passport & for configuration
-
+//Connect to the database
+let configDB = require('./lib/dbconfig.js');
+mongoose.connect(configDB.url);
+// load the auth variables
+const configAuth = require('./lib/auth');
+// load the mongoose User variables
+let user = require('./app/models/user');
+// pass passport & for configuration
+require('./lib/passport')(passport, configAuth, user);
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
-
 //Setting our Template Engine to EJS
 app.set('view engine', 'ejs'); // set up ejs for templating
 app.use(express.static(__dirname + '/public'));
-//app.use('/vendor', express.static(__dirname + '/bower_components'));
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 
 // required for passport
@@ -46,9 +43,8 @@ app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
-
 //Passing our Passport Information and Application Information to our routes
-require('./app/routes.js')(app, passport);
+require('./app/routes.js')(app, passport, user);
 
 app.listen(port);
 console.log('Server connection established');
--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,124 @@
 "use strict";
 module.exports = function(app, passport) {
 
-  app.get('/', function(req, res) {
-    res.render('index.ejs', {
-            title: "Hello There"
-        }); // load the login.ejs file
-      });
+    // ----- Basic Routes
 
+    app.get('/', function(req, res) {
+        res.render('index.ejs', {
+            title: "Social Network Intergration Search Application"
+        }); // load the login.ejs file
+    });
+
+    app.get('/profile', isLoggedIn, function(req, res) {
+        res.render('profile.ejs', {
+            user: req.user
+        });
+    });
+
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+
+    // ----- Facebook Routes
+
+    app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
+    //When the facebook route confirms, we handle the re-direct here
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect: '/profile',
+            failureRedirect: '/'
+        }));
+    app.get('/unlink/facebook', function(req, res) {
+        let user = req.user;
+        user.facebook.token = undefined;
+        user.save(function(err) {
+            if (err) {
+                throw err;
+            }
+            res.redirect('/profile');
+        });
+    });
+
+    app.get('/connect/facebook', passport.authorize('facebook', {
+        scope: 'email'
+    }));
+
+    app.get('/connect/facebook/callback',
+        passport.authorize('facebook', {
+            successRedirect: '/profile',
+            failureRedirect: '/'
+        }));
+
+
+    // ----- Twitter Routes
+
+    app.get('/auth/twitter', passport.authenticate('twitter'));
+    // handle the callback after twitter has authenticated the user
+    app.get('/auth/twitter/callback',
+        passport.authenticate('twitter', {
+            successRedirect: '/profile',
+            failureRedirect: '/'
+        }));
+    app.get('/unlink/twitter', function(req, res) {
+        let user = req.user;
+        user.twitter.token = undefined;
+        user.save(function(err) {
+            if (err) {
+                throw err;
+            }
+            res.redirect('/profile');
+        });
+    });
+
+    app.get('/connect/twitter', passport.authorize('twitter', {
+        scope: 'email'
+    }));
+
+    app.get('/connect/twitter/callback',
+        passport.authorize('twitter', {
+            successRedirect: '/profile',
+            failureRedirect: '/'
+        }));
+
+
+    // ----- Instagram Routes
+
+    app.get('/auth/instagram', passport.authenticate('instagram'));
+
+    app.get('/auth/instagram/callback',
+        passport.authenticate('instagram', {
+            successRedirect: '/profile',
+            failureRedirect: '/'
+        }));
+
+    app.get('/unlink/instagram', function(req, res) {
+        let user = req.user;
+        user.instagram.token = undefined;
+
+        user.save(function(err) {
+            if (err) {
+                throw err;
+            }
+            res.redirect('/profile');
+        });
+    });
+
+    app.get('/connect/instagram', passport.authenticate('instagram'));
+
+    app.get('/connect/instagram/callback',
+        passport.authorize('instagram', {
+            successRedirect: '/profile',
+            failureRedirect: '/'
+        }));
+
+
+    // ------ Other Routes
+    function isLoggedIn(req, res, next) {
+        if (req.isAuthenticated()) {
+            return next();
+        }
+        res.redirect('/');
+    }
 };

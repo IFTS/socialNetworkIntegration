124baaca3034661fb31498d91d3117d967c6ff90
Nick Liffen <UP653591@myport.ac.uk>
2016-03-14 15:41:46 +0000

    Search working in Console

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,11 @@ module.exports = function(app, passport, fbgraph, Twitter, ig, moment) {
 
     // ----- Basic Routes
 
+    //For Twitter we are just searching .text
+    //For InstaGram we are searching .caption.text
+
+
+
     app.get('/', function(req, res) {
         res.render('index.ejs', {
             title: "Social Network Intergration Search Application"
@@ -10,139 +15,139 @@ module.exports = function(app, passport, fbgraph, Twitter, ig, moment) {
     });
 
     app.get('/profile', isLoggedIn, function(req, res) {
-      var twitterFeed = [];
-      //var facebookFeed = [];
-      var instaFeed = [];
-      var googleFeed = [];
-      var twitter, instagram, google;
+        var twitterFeed = [];
+        //var facebookFeed = [];
+        var instaFeed = [];
+        var googleFeed = [];
+        var twitter, instagram, google;
 
-      if (req.user.twitter) {
-        twitter = new Twitter({
-          consumer_key: process.env.TwitterAppID,
-          consumer_secret: process.env.TwitterAppSecret,
-          access_token_key: req.user.twitter.token,
-          access_token_secret: req.user.twitter.tokenSecret
-        });
-         twitter = new Promise((resolve) => {
-          twitter.get('statuses/user_timeline', function(error, tweets){
-            if (error) {
-              console.error(error);
-              resolve();
-            }
-            else {
-              twitterFeed = tweets;
-              resolve();
-            }
-          });
-        });
-      }
-
-      if (req.user.instagram) {
-        instagram = new Promise((resolve) => {
-          ig.use({
-              access_token: req.user.instagram.token,
-              client_id: process.env.InstagramAppID,
-              client_secret: process.env.InstagramAppSecret
+        if (req.user.twitter) {
+            twitter = new Twitter({
+                consumer_key: process.env.TwitterAppID,
+                consumer_secret: process.env.TwitterAppSecret,
+                access_token_key: req.user.twitter.token,
+                access_token_secret: req.user.twitter.tokenSecret
             });
-          ig.user_media_recent(req.user.instagram.id, function(err, medias) {
-            if (err) {
-              console.error(err);
-              resolve();
-            }
-            else {
-              instaFeed = medias;
-              resolve();
-            }
+            twitter = new Promise((resolve) => {
+                twitter.get('statuses/user_timeline', function(error, tweets) {
+                    if (error) {
+                        console.error(error);
+                        resolve();
+                    } else {
+                        twitterFeed = tweets;
+                        resolve();
+                    }
+                });
             });
-        });
-      }
-      if (req.user.google) {
-        google = new Promise((resolve) => {
-          googleFeed = req.user.google;
-          resolve();
-        });
-      }
-
-      Promise.all([twitter, instagram, google]).then(() => {
-        for (var i = 0; i < twitterFeed.length; i++) {
-          twitterFeed[i].time = moment.utc(twitterFeed[i].created_at).format();
-          twitterFeed[i].timeFormat = moment.utc(twitterFeed[i].created_at).format('LLL');
-          twitterFeed[i].api = 'twitter';
-        }
-        for (var j = 0; j < instaFeed.length; j++) {
-          instaFeed[j].time = moment.unix(instaFeed[j].created_time).format();
-          instaFeed[j].timeFormat = moment.unix(instaFeed[j].created_time).format('LLL');
-          instaFeed[j].api = 'instagram';
         }
 
-        if (i === twitterFeed.length && j === instaFeed.length) {
-          if (instaFeed.length === 0 || twitterFeed.length === 0) {
-            res.render('profile.ejs', {
-                user: req.user,
-                twitterFeed: twitterFeed,
-                instaFeed: instaFeed,
-                googleFeed: googleFeed,
-                merged: []
+        if (req.user.instagram) {
+            instagram = new Promise((resolve) => {
+                ig.use({
+                    access_token: req.user.instagram.token,
+                    client_id: process.env.InstagramAppID,
+                    client_secret: process.env.InstagramAppSecret
+                });
+                ig.user_media_recent(req.user.instagram.id, function(err, medias) {
+                    if (err) {
+                        console.error(err);
+                        resolve();
+                    } else {
+                        instaFeed = medias;
+                        resolve();
+                    }
+                });
             });
-          }
-          else {
-            mergeObjs(twitterFeed, instaFeed, [], 0, 0, function (result) {
-            let resultStringified = JSON.stringify(result);
-            let resultParsed      = JSON.parse(resultStringified);
-              res.render('profile.ejs', {
-                  user: req.user,
-                  merged: resultParsed
-              });
-            });
-          }
         }
-        // else if (twitterFeed.length == 0 && instaFeed.length != 0) {
-        //   res.render('profile.ejs', {
-        //       user: req.user,
-        //       twitterFeed: twitterFeed,
-        //       instaFeed: instaFeed,
-        //       googleFeed: googleFeed,
-        //   });
-        // }
-        // else if (twitterFeed.length != 0 && instaFeed.length == 0) {
-        //   res.render('profile.ejs', {
-        //       user: req.user,
-        //       twitterFeed: twitterFeed,
-        //       instaFeed: instaFeed,
-        //       googleFeed: googleFeed,
-        //   });
-        // }
-        // res.render('profile.ejs', {
-        //     user: req.user,
-        //     twitterFeed: twitterFeed,
-        //     instaFeed: instaFeed,
-        //     googleFeed: googleFeed,
-        // });
-      });
+        if (req.user.google) {
+            google = new Promise((resolve) => {
+                googleFeed = req.user.google;
+                resolve();
+            });
+        }
+        Promise.all([twitter, instagram, google]).then(() => {
+            for (var i = 0; i < twitterFeed.length; i++) {
+                twitterFeed[i].time = moment.utc(twitterFeed[i].created_at).format();
+                twitterFeed[i].timeFormat = moment.utc(twitterFeed[i].created_at).format('LLL');
+                twitterFeed[i].api = 'twitter';
+            }
+            for (var j = 0; j < instaFeed.length; j++) {
+                instaFeed[j].time = moment.unix(instaFeed[j].created_time).format();
+                instaFeed[j].timeFormat = moment.unix(instaFeed[j].created_time).format('LLL');
+                instaFeed[j].api = 'instagram';
+            }
+
+            if (i === twitterFeed.length && j === instaFeed.length) {
+                if (instaFeed.length === 0 || twitterFeed.length === 0) {
+                    res.render('profile.ejs', {
+                        user: req.user,
+                        twitterFeed: twitterFeed,
+                        instaFeed: instaFeed,
+                        googleFeed: googleFeed,
+                        merged: []
+                    });
+                } else {
+                    mergeObjs(twitterFeed, instaFeed, [], 0, 0, function(result) {
+                        let resultStringified = JSON.stringify(result);
+                        let resultParsed = JSON.parse(resultStringified);
+                        //res.send(resultParsed);
+                        if (Object.keys(req.body).length === 0) {
+                          res.render('profile.ejs', {
+                              user: req.user,
+                              merged: resultParsed
+                          });
+                        } else if(req.body.location === 'client'){
+                          res.send(resultParsed);
+                        }
+                        else{
+                          res.send("Something's Wrong");
+                        }
+                    });
+                }
+            }
+            // else if (twitterFeed.length == 0 && instaFeed.length != 0) {
+            //   res.render('profile.ejs', {
+            //       user: req.user,
+            //       twitterFeed: twitterFeed,
+            //       instaFeed: instaFeed,
+            //       googleFeed: googleFeed,
+            //   });
+            // }
+            // else if (twitterFeed.length != 0 && instaFeed.length == 0) {
+            //   res.render('profile.ejs', {
+            //       user: req.user,
+            //       twitterFeed: twitterFeed,
+            //       instaFeed: instaFeed,
+            //       googleFeed: googleFeed,
+            //   });
+            // }
+            // res.render('profile.ejs', {
+            //     user: req.user,
+            //     twitterFeed: twitterFeed,
+            //     instaFeed: instaFeed,
+            //     googleFeed: googleFeed,
+            // });
+        });
 
     });
 
-    function mergeObjs (a, b, c, numA, numB, callback) {
+    function mergeObjs(a, b, c, numA, numB, callback) {
 
-      if (numA === a.length && numB === b.length) {
-        callback(c);
-      }
-      else if (numA === a.length && numB < b.length) {
-        c.push(b[numB]);
-        mergeObjs(a, b, c, numA, numB+1, callback);
-      }
-      else if (numB === b.length && numA < a.length) {
-        c.push(a[numA]);
-        mergeObjs(a, b, c, numA+1, numB, callback);
-      }
-      else if (a[numA].time >= b[numB].time) {
-        c.push(a[numA]);
-        mergeObjs(a, b, c, numA+1, numB, callback);
-      }
-      else {
-        c.push(b[numB]);
-        mergeObjs(a, b, c, numA, numB+1, callback);
-      }
+        if (numA === a.length && numB === b.length) {
+            callback(c);
+        } else if (numA === a.length && numB < b.length) {
+            c.push(b[numB]);
+            mergeObjs(a, b, c, numA, numB + 1, callback);
+        } else if (numB === b.length && numA < a.length) {
+            c.push(a[numA]);
+            mergeObjs(a, b, c, numA + 1, numB, callback);
+        } else if (a[numA].time >= b[numB].time) {
+            c.push(a[numA]);
+            mergeObjs(a, b, c, numA + 1, numB, callback);
+        } else {
+            c.push(b[numB]);
+            mergeObjs(a, b, c, numA, numB + 1, callback);
+        }
     }
 
     app.get('/logout', function(req, res) {
@@ -152,7 +157,9 @@ module.exports = function(app, passport, fbgraph, Twitter, ig, moment) {
 
     // ----- Facebook Routes
 
-    app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email','user_posts'] }));
+    app.get('/auth/facebook', passport.authenticate('facebook', {
+        scope: ['email', 'user_posts']
+    }));
 
     //When the facebook route confirms, we handle the re-direct here
     app.get('/auth/facebook/callback',
@@ -216,21 +223,21 @@ module.exports = function(app, passport, fbgraph, Twitter, ig, moment) {
         }));
 
     app.get('/get/twitter/timeline', isLoggedIn, (req, res) => {
-      var twitter = new Twitter({
-        consumer_key: process.env.TwitterAppID,
-        consumer_secret: process.env.TwitterAppSecret,
-        access_token_key: req.user.twitter.token,
-        access_token_secret: req.user.twitter.tokenSecret
-      });
-      twitter.get('statuses/home_timeline', function(error, tweets){
-        if (error){
-          console.error(error);
-        }
-        res.render('profile.ejs', {
-            user: req.user,
-            twitterFeed: tweets
+        var twitter = new Twitter({
+            consumer_key: process.env.TwitterAppID,
+            consumer_secret: process.env.TwitterAppSecret,
+            access_token_key: req.user.twitter.token,
+            access_token_secret: req.user.twitter.tokenSecret
         });
-      });
+        twitter.get('statuses/home_timeline', function(error, tweets) {
+            if (error) {
+                console.error(error);
+            }
+            res.render('profile.ejs', {
+                user: req.user,
+                twitterFeed: tweets
+            });
+        });
     });
 
 
@@ -265,37 +272,37 @@ module.exports = function(app, passport, fbgraph, Twitter, ig, moment) {
         }));
 
     // ------ Google Routes
-    app.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
+    app.get('/auth/google', passport.authenticate('google', {
+        scope: ['profile', 'email']
+    }));
 
     app.get('/auth/google/callback',
         passport.authenticate('google', {
-          successRedirect: '/profile',
-          failureRedirect: '/'
+            successRedirect: '/profile',
+            failureRedirect: '/'
         }));
 
-        app.get('/unlink/google', function(req, res) {
-            let user = req.user;
-            user.google.token = undefined;
+    app.get('/unlink/google', function(req, res) {
+        let user = req.user;
+        user.google.token = undefined;
 
-            user.save(function(err) {
-                if (err) {
-                    throw err;
-                }
-                res.redirect('/profile');
-            });
+        user.save(function(err) {
+            if (err) {
+                throw err;
+            }
+            res.redirect('/profile');
         });
+    });
 
-        app.get('/connect/google', passport.authenticate('google', {scope: ['profile', 'email']}));
+    app.get('/connect/google', passport.authenticate('google', {
+        scope: ['profile', 'email']
+    }));
 
-        app.get('/connect/google/callback',
-            passport.authorize('google', {
-                successRedirect: '/profile',
-                failureRedirect: '/'
-            }));
-
-
-
-
+    app.get('/connect/google/callback',
+        passport.authorize('google', {
+            successRedirect: '/profile',
+            failureRedirect: '/'
+        }));
     // ------ Other Routes
     function isLoggedIn(req, res, next) {
         if (req.isAuthenticated()) {
@@ -303,4 +310,121 @@ module.exports = function(app, passport, fbgraph, Twitter, ig, moment) {
         }
         res.redirect('/');
     }
+
+    app.post('/profile', isLoggedIn, function(req, res) {
+        var twitterFeed = [];
+        //var facebookFeed = [];
+        var instaFeed = [];
+        var googleFeed = [];
+        var twitter, instagram, google;
+
+        if (req.user.twitter) {
+            twitter = new Twitter({
+                consumer_key: process.env.TwitterAppID,
+                consumer_secret: process.env.TwitterAppSecret,
+                access_token_key: req.user.twitter.token,
+                access_token_secret: req.user.twitter.tokenSecret
+            });
+            twitter = new Promise((resolve) => {
+                twitter.get('statuses/user_timeline', function(error, tweets) {
+                    if (error) {
+                        console.error(error);
+                        resolve();
+                    } else {
+                        twitterFeed = tweets;
+                        resolve();
+                    }
+                });
+            });
+        }
+
+        if (req.user.instagram) {
+            instagram = new Promise((resolve) => {
+                ig.use({
+                    access_token: req.user.instagram.token,
+                    client_id: process.env.InstagramAppID,
+                    client_secret: process.env.InstagramAppSecret
+                });
+                ig.user_media_recent(req.user.instagram.id, function(err, medias) {
+                    if (err) {
+                        console.error(err);
+                        resolve();
+                    } else {
+                        instaFeed = medias;
+                        resolve();
+                    }
+                });
+            });
+        }
+        if (req.user.google) {
+            google = new Promise((resolve) => {
+                googleFeed = req.user.google;
+                resolve();
+            });
+        }
+        Promise.all([twitter, instagram, google]).then(() => {
+            for (var i = 0; i < twitterFeed.length; i++) {
+                twitterFeed[i].time = moment.utc(twitterFeed[i].created_at).format();
+                twitterFeed[i].timeFormat = moment.utc(twitterFeed[i].created_at).format('LLL');
+                twitterFeed[i].api = 'twitter';
+            }
+            for (var j = 0; j < instaFeed.length; j++) {
+                instaFeed[j].time = moment.unix(instaFeed[j].created_time).format();
+                instaFeed[j].timeFormat = moment.unix(instaFeed[j].created_time).format('LLL');
+                instaFeed[j].api = 'instagram';
+            }
+
+            if (i === twitterFeed.length && j === instaFeed.length) {
+                if (instaFeed.length === 0 || twitterFeed.length === 0) {
+                    res.render('profile.ejs', {
+                        user: req.user,
+                        twitterFeed: twitterFeed,
+                        instaFeed: instaFeed,
+                        googleFeed: googleFeed,
+                        merged: []
+                    });
+                } else {
+                    mergeObjs(twitterFeed, instaFeed, [], 0, 0, function(result) {
+                        let resultStringified = JSON.stringify(result);
+                        let resultParsed = JSON.parse(resultStringified);
+                        console.log(req.body);
+                        if (Object.keys(req.body).length === 0) {
+                          res.render('profile.ejs', {
+                              user: req.user,
+                              merged: resultParsed
+                          });
+                        } else if(req.body.location === 'client'){
+                          res.send(resultParsed);
+                        }
+                        else{
+                          res.send("Something's Wrong");
+                        }
+                    });
+                }
+            }
+            // else if (twitterFeed.length == 0 && instaFeed.length != 0) {
+            //   res.render('profile.ejs', {
+            //       user: req.user,
+            //       twitterFeed: twitterFeed,
+            //       instaFeed: instaFeed,
+            //       googleFeed: googleFeed,
+            //   });
+            // }
+            // else if (twitterFeed.length != 0 && instaFeed.length == 0) {
+            //   res.render('profile.ejs', {
+            //       user: req.user,
+            //       twitterFeed: twitterFeed,
+            //       instaFeed: instaFeed,
+            //       googleFeed: googleFeed,
+            //   });
+            // }
+            // res.render('profile.ejs', {
+            //     user: req.user,
+            //     twitterFeed: twitterFeed,
+            //     instaFeed: instaFeed,
+            //     googleFeed: googleFeed,
+            // });
+        });
+
+    });
 };
--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,30 @@
 "use strict";
 
-//  ------ IS THIS NEEDED
-/*
-const createTwitterTimeline = (timeline) => {
-  const twitterTimelineList = document.querySelector('.twitter-timeline');
-
-  for (let i = 0; i < timeline.length; i++){
-    const item = timeline[i];
-
-    var li = document.createElement('li');
-    li.innerHTML = item;
-    twitterTimelineList.appendChild(li);
-  }
-};
-document.addEventListener('DOMContentLoaded', load, false);
-*/
-
 $("#goToPrefrences").click(function() {
     $('html, body').animate({
         scrollTop: $("#prefrences").offset().top
     }, 2000);
 });
+
+let info = {
+    location: "client"
+};
+
+$.ajax({
+    type: 'POST',
+    data: info,
+    url: '/profile',
+    dataType: 'JSON'
+}).done(function(response) {
+    searchFunction(response);
+});
+
+function searchFunction(objToSearch) {
+    $("#myInput").keyup(function() {
+        console.log("were inside keyup");
+        let text = this.value;
+        let search = '';
+        search = JSON.search(objToSearch, `//*[contains(text, "${text}") or contains(caption, "${text}")]`);
+        console.log(search);
+    });
+}
